1801acb7715898abcc70543147ecd326121be8f2
CEOToanNguyen <nmtoan58@gmail.com>
2021-04-25 17:02:04 +0700

    upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var setting = document.getElementById('setting');
+var cart = document.getElementById('cart-list');
+
+function getCart () {
+    cart.classList.toggle("hide-cart");
+}
+
+function getSetting() {
+    setting.classList.toggle("hide-setting");
+}
+
+// if(cart.style.display = 'block'){
+//     setting.style.display = 'none';
+// }
+
+// else if(setting.style.display = ){
+//     setting.style.display = 
+// }
