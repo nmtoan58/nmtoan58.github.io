df60349f4e0f83e5a5d9f7cabd13d12d977f287f
CEOToanNguyen <nmtoan58@gmail.com>
2021-05-04 20:11:04 +0700

    upload

---
+++ projects/MEDYPLUS/flag.js
@@ -0,0 +1,6 @@
+var imgFlag = document.getElementById('img-flag').getAttribute('src');
+var selectFlag = document.getElementById('select-flag').value;
+
+function flagImg() {   
+  console.log('a');
+}
